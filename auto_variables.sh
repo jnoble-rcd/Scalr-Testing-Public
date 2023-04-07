0aaf3345e839349f257dae23b714bcc8a5ca3d2b
jnoble-rcd <72411819+jnoble-rcd@users.noreply.github.com>
2023-04-07 10:54:45 -0400

    Create auto_variables.sh

---
+++ auto_variables.sh
@@ -0,0 +1,41 @@
+modulename=$(ls ./.terraform/modules/ | grep -v 'modules' | tr ' ' '\n' | cut -f1 -d"." | uniq)
+
+##check applicaiton variable tf
+appvarsfile="../application_variables.tf"
+if [[ -f "$appvarsfile" ]]; then
+    echo "file exists"
+    cp ../application_variables.tf ./
+    appvarsjson=$(cat ../application_variables.tf | ./hcl2json_darwin_amd64)
+    appvarsjsonnames=($(jq --raw-output '.[]| keys[]' <<< $appvarsjson))
+fi
+
+### deployment variable tf
+
+varsjson=$(cat .terraform/modules/$modulename/variables.tf | ./hcl2json_darwin_amd64)
+variablenames=($(jq --raw-output '.[]| keys[]' <<< $varsjson))
+#echo ${variablenames[*]}
+test=$(cat << 'EOF'
+variable "unmatchablestring123" {}
+EOF
+)
+
+if [[ -f "$appvarsfile" ]]; then
+    #echo ${variablenames[*]}
+    echo ${variablenames[@]}
+    #echo ${appvarsjsonnames[@]}
+    uniquevars=$(echo ${variablenames[@]} ${appvarsjsonnames[@]} | tr ' ' '\n' | sort | uniq -u)
+    #echo ${uniquevars[*]}
+    echo "1"
+else
+    uniquevars=${variablenames[*]}
+    echo ${uniquevars[*]}
+    echo "2"
+fi
+#echo $uniquevars
+for variable in ${uniquevars[*]}
+    do
+        updatedvariabletext=$(echo $test | sed "s~unmatchablestring123~$variable~g")
+        echo $updatedvariabletext >> autovariables.tf
+    done 
+
+
