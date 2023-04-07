6f1242f6fb7eae53f73d72ac7db21efbcc85bdf8
jnoble-rcd <72411819+jnoble-rcd@users.noreply.github.com>
2023-04-07 13:44:33 -0400

    Update auto_variables.sh

--- auto_variables.sh
+++ auto_variables.sh
@@ -1,20 +1,30 @@
-
-appvarsfile="./terraform.tfvars.json"
-if [[ -f "$appvarsfile" ]]; then
-    echo "1"
-    scalrtfvarjson=$(cat ./terraform.tfvars.json)
-    scalrtfvarnames=($(jq --raw-output 'keys[]' <<< $scalrtfvarjson))
-    cat ${scalrtfvarnames[*]}
-    echo "2"
-fi
-
+##check applicaiton variable tf
 ### deployment variable tf
 test=$(cat << 'EOF'
 variable "unmatchablestring123" {}
 EOF
 )
 
-for variable in ${scalrtfvarnames[*]}
+appvarsfile="./terraform.tfvars.json"
+if [[ -f "$appvarsfile" ]]; then
+    echo "1"
+    scalrtfvarjson=$(cat ./terraform.tfvars.json)
+    scalrtfvarnames=($(jq --raw-output 'keys[]' <<< $scalrtfvarjson))
+    echo "2"
+    #echo ${variablenames[*]}
+    #echo ${appvarsjsonnames[@]}
+    if [[ -f "../application.tfvars" ]]; then
+        echo "fileexists"
+        applicationtfvarnames=$(cat ../application.tfvars | tr ' ' '\n' | sort | uniq -u)
+        uniquevars=$(echo ${scalrtfvarnames[@]} ${applicationtfvarnames[@]} | tr ' ' '\n' | sort | uniq -u)
+    else
+        uniquevars=${scalrtfvarnames[*]}
+    fi
+
+fi
+
+
+for variable in ${uniquevars[*]}
     do
         updatedvariabletext=$(echo $test | sed "s~unmatchablestring123~$variable~g")
         echo $updatedvariabletext >> autovariables.tf
